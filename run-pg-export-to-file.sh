99a1a6e9529b0efa45fbd3a5e520a92467be39cb
andre-carvalho <afacarvalho@yahoo.com.br>
2024-06-06 09:42:29 -0300

    Redefine output location and fix error

--- run-pg-export-to-file.sh
+++ run-pg-export-to-file.sh
@@ -2,7 +2,7 @@
 #
 # Need a directory to use as a docker volume where output data is written.
 # Adjust the path in VOLUME_HOST if necessary.
-VOLUME_HOST="/main/storage/exported/files"
+VOLUME_HOST=""
 #
 # detect the location of this script
 SCRIPT_DIR=$(pwd)
@@ -25,18 +25,22 @@ fi;
 # If a new image with a different tag was created, change the IMAGE_TAG
 IMAGE_TAG="v1.0.0"
 
-if [[ "$2" != "up" && "$1" != "down" ]]; then
+if [[ "$2" != "up" && "$2" != "down" ]]; then
   echo "Use up to start OR down to stop."
   echo "------------------------------------------"
   echo "Example: ./run-pg-export-to-file.sh vector up"
 
 else
+  if [[ "${VOLUME_HOST}" = "" ]]; then
+    VOLUME_HOST="${SCRIPT_DIR}/${DIR_TYPE}"
+  fi;
+
   if [[ "$2" == "up" ]]; then
     docker run -d --name export_pg_to_${1}_file --rm -v ${SCRIPT_DIR}/${DIR_TYPE}:/scripts \
     -v ${VOLUME_HOST}:/main/storage/exported/files \
     terrabrasilis/run-scripts-pg-gdal-3-6:${IMAGE_TAG} bash /scripts/start.sh
   fi;
   if [[ "$2" == "down" ]]; then
-    docker container stop export_pg_${1}_to_file
+    docker container stop export_pg_to_${1}_file
   fi;
 fi;
