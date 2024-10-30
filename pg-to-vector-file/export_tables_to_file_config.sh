8a93652fc2dc357bc6fb62e371b7e369cb5e7e92
andre-carvalho <afacarvalho@yahoo.com.br>
2024-10-30 18:30:16 -0300

    Change configs to export vertors

--- pg-to-vector-file/export_tables_to_file_config.sh
+++ pg-to-vector-file/export_tables_to_file_config.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
+#
+# Used as a database name suffix. Consider that the default database name is prodes_<biome>_nb_p<BASE_YEAR>
+BASE_YEAR="2024"
+#
 # if you want export only filtered tables, set the table names into FILTER variable below and removing the character # to uncomment that.
 # FILTER=("table_name_a" "table_name_b" "table_name_c")
 # FILTER=("forest" "accumulated_deforestation_2007" "hydrography" "cloud" "no_forest" "residual" "yearly_deforestation" )
 # FILTER=("accumulated_deforestation_2007_biome" "hydrography_biome" "no_forest_biome" "residual_biome" "yearly_deforestation_biome" "yearly_deforestation_nf_biome" "yearly_deforestation_smaller_than_625ha_biome")
-# FILTER=("cloud_2023_pri" "cloud_2023_pri_biome" "residual_2023_pri" "residual_2023_pri_biome" "yearly_deforestation_2023_pri" "yearly_deforestation_2023_pri_biome" "yearly_deforestation_smaller_than_625ha_2023_pri_biome")
-FILTER=("accumulated_deforestation_2000" "hydrography" "cloud" "residual" "yearly_deforestation")
+#FILTER=("residual_${BASE_YEAR}_pri" "residual_${BASE_YEAR}_pri_biome" "yearly_deforestation_${BASE_YEAR}_pri" "yearly_deforestation_${BASE_YEAR}_pri_biome" "yearly_deforestation_smaller_than_625ha_${BASE_YEAR}_pri")
+#FILTER=("accumulated_deforestation_2000" "hydrography" "cloud" "residual" "yearly_deforestation")
 #
 # Used for cloud table or forest table, generate one shape of each class_name/year to avoid the limit of maximum size of shapefiles
 BREAK_SHP=("forest" "cloud" "forest_biome" "cloud_biome")
@@ -13,9 +16,7 @@ BREAK_SHP=("forest" "cloud" "forest_biome" "cloud_biome")
 #
 # used to confirm the sub_class column only for the deforestation tables in the list below (valid for "amazonia" and "amazonia_legal")
 TABLES_WITH_SUBCLASS=("yearly_deforestation" "yearly_deforestation_biome")
-#
-# Used as a database name suffix. Consider that the default database name is prodes_<biome>_nb_p<BASE_YEAR>
-BASE_YEAR="2023"
+# TABLES_WITH_SUBCLASS=("yearly_deforestation_${BASE_YEAR}_pri" "yearly_deforestation_${BASE_YEAR}_pri_biome")
 #
 # Set the output directory (is mapped inside the container after run)
 BASE_PATH_DATA="/main/storage/exported/files"
@@ -25,10 +26,10 @@ BASE_PATH_DATA="/main/storage/exported/files"
 RM_OUT="no"
 #
 # Fix geometries before export
-FIX="yes"
+FIX="no"
 #
 # Fix FID before export. Make an update into sequential numeric column used as primary key.
-FID="no"
+FID="yes"
 #
 # Export to Shapefile
 SHP="yes"
@@ -44,4 +45,4 @@ TABLE_TYPE='BASE TABLE'
 #
 # list of biomes to export data
 # PRODES_DBS=("pampa" "caatinga" "pantanal" "mata_atlantica" "cerrado" "amazonia" "amazonia_legal")
-PRODES_DBS=("mata_atlantica")
+PRODES_DBS=("cerrado")
